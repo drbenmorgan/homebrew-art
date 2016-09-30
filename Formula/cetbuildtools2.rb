9e656e4a89a9bed28b9b8de2273d2a7e2011bfb7
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-09-30 23:25:21 +0100

    cetbuildtools2: default build docs

--- Formula/cetbuildtools2.rb
+++ Formula/cetbuildtools2.rb
@@ -1,13 +1,16 @@
 class Cetbuildtools2 < Formula
   desc "Custom CMake functionality for building Art Software Stack"
   homepage "https://github.com/drbenmorgan/cetbuildtools2"
-  url "https://github.com/drbenmorgan/cetbuildtools2", :branch => "develop", :revision => "9b4993b"
+  url "https://github.com/drbenmorgan/cetbuildtools2.git", :branch => "develop", :revision => "9b4993b"
   version "0.2.0"
 
   depends_on "cmake"
+  depends_on "sphinx-doc" => :recommended
 
   def install
     mkdir "build" do
+      args = std_cmake_args
+      args << "-DSPHINX_BUILD_HTML=ON" if build.with? "sphinx-doc"
       system "cmake", "../", *args
       system "make", "install"
     end
@@ -15,6 +18,6 @@ class Cetbuildtools2 < Formula
 
   test do
     (testpath/"CMakeLists.txt").write("find_package(cetbuildtools2)")
-    system "#{bin}/cmake", "."
+    system "cmake", "."
   end
 end
