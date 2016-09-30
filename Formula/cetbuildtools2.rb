694fc1d16e2ab4ddb95aa3502fd6644f8fd59906
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2016-09-30 22:57:11 +0100

    cetbuildtools2: new formula
    CMake custom modules for building CET/Art based packages

---
+++ Formula/cetbuildtools2.rb
@@ -0,0 +1,19 @@
+class Cetbuildtools2 < Formula
+  desc "Custom CMake functionality for building Art Software Stack"
+  homepage "https://github.com/drbenmorgan/cetbuildtools2"
+  url "https://github.com/drbenmorgan/cetbuildtools2", :branch => "develop", :revision => "9b4993b"
+  version "0.2.0"
+
+  depends_on "cmake"
+
+  def install
+    mkdir "build" do
+      system "cmake", "../", *args
+      system "make", "install"
+  end
+
+  test do
+    (testpath/"CMakeLists.txt").write("find_package(cetbuildtools2)")
+    system "#{bin}/cmake", "."
+  end
+end
