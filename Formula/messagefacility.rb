74422d8a7e5c613989d4940921e1793af9cbcb96
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-06-26 15:47:34 +0100

    messagefacility: bump version to 2.0.2
    Add dependency on TBB.

--- Formula/messagefacility.rb
+++ Formula/messagefacility.rb
@@ -1,16 +1,19 @@
 class Messagefacility < Formula
   desc "FNAL messagefacility C++ library"
   homepage "https://github.com/drbenmorgan/fnal-messagefacility"
-  url "https://github.com/drbenmorgan/fnal-messagefacility.git", :branch => "feature/alt-cmake-macos", :revision => "534a452"
-  version "1.17.2"
+  url "https://github.com/drbenmorgan/fnal-messagefacility.git", :branch => "feature/alt-cmake"
+  version "2.0.2"
 
   depends_on "cmake" => :build
   depends_on "cetbuildtools2"
   depends_on "cetlib"
+  depends_on "cetlib_except"
   depends_on "fhicl-cpp"
   #depends_on "boost" => "c++11"
   depends_on "sqlite"
+  depends_on "tbb" => ["--c++11"]
   depends_on "doxygen" => :optional
+
 
   def install
     mkdir "build" do
