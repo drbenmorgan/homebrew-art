3cdbbaa4b7514c465633deb34fc4ba455eaad52e
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-10-25 14:43:36 +0100

    cetbuildtools2: Bump version to 0.2.0

--- Formula/cetbuildtools2.rb
+++ Formula/cetbuildtools2.rb
@@ -1,17 +1,18 @@
 class Cetbuildtools2 < Formula
-  desc "Custom CMake functionality for building Art Software Stack"
+  desc "Custom CMake functionality for building Art Software Suite"
   homepage "https://github.com/drbenmorgan/cetbuildtools2"
-  url "https://github.com/drbenmorgan/cetbuildtools2.git", :branch => "develop", :revision => "9b4993b"
-  version "0.2.0"
+  url "https://github.com/drbenmorgan/cetbuildtools2.git", :tag => "v0.2.0"
+  head "https://github.com/drbenmorgan/cetbuildtools2.git", :branch => "develop"
 
-  depends_on "cmake"
-  depends_on "sphinx-doc" => :recommended
+  depends_on "cmake" => [:build, :run]
+  depends_on "sphinx-doc" => [:recommended, :build]
 
   def install
     mkdir "build" do
       args = std_cmake_args
       args << "-DSPHINX_BUILD_HTML=ON" if build.with? "sphinx-doc"
       system "cmake", "../", *args
+      system "ctest"
       system "make", "install"
     end
   end
