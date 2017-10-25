0ee034c0108870ed56d89efb2ffd0189d2e54f8c
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2017-10-25 15:15:53 +0100

    cetlib_except: new formula

---
+++ Formula/cetlib_except.rb
@@ -0,0 +1,40 @@
+class CetlibExcept < Formula
+  desc "Cet C++ exception classes"
+  homepage "https://github.com/drbenmorgan/fnal-cetlib_except"
+  url "https://github.com/drbenmorgan/fnal-cetlib_except.git", :branch => "feature/alt-cmake"
+  version "1.1.3"
+  head "https://github.com/drbenmorgan/fnal-cetlib_except.git", :branch => "feature/alt-cmake"
+
+  depends_on "cetbuildtools2" => :build
+  depends_on "cmake" => :build
+  depends_on "doxygen" => [:build, :recommended]
+
+  needs :cxx14
+
+  def install
+    mkdir "build" do
+      args = std_cmake_args
+      args << "-DALT_CMAKE=ON"
+      system "cmake", "..", *args
+      system "make"
+      system "ctest"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <cetlib_except/exception.h>
+      int main() {
+        try {
+          throw cet::exception("HomebreTestException");
+        }
+        catch (cet::exception& e) {
+        }
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++1y", "test.cpp", "-L#{lib}", "-lcetlib_except", "-o", "test"
+    system "./test"
+  end
+end
