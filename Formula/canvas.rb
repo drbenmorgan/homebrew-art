cbbef8ecaf0145598db512e50bc1670736155d82
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-01 15:42:45 +0000

    canvas: New formula

---
+++ Formula/canvas.rb
@@ -0,0 +1,45 @@
+class Canvas < Formula
+  desc "FNAL C++ library for I/O"
+  homepage "https://github.com/drbenmorgan/fnal-canvas.git"
+  url "https://github.com/drbenmorgan/fnal-canvas.git", :branch => "feature/alt-cmake"
+  version "3.2.1"
+  head "https://github.com/drbenmorgan/fnal-canvas.git", :branch => "feature/alt-cmake"
+
+  depends_on "drbenmorgan/art_suite/cetbuildtools2"
+  depends_on "drbenmorgan/art_suite/cetlib_except"
+  depends_on "drbenmorgan/art_suite/cetlib"
+  depends_on "drbenmorgan/art_suite/fhicl-cpp"
+  depends_on "drbenmorgan/art_suite/messagefacility"
+  depends_on "art-clhep"
+  # This leads to an audit error, so should package own boost!
+  depends_on "boost" => "c++11"
+  depends_on "tbb" => "c++11"
+  depends_on "cmake" => :build
+  depends_on "root6" if OS.mac?
+  depends_on "range-v3"
+  depends_on "doxygen" => [:recommended, :build]
+
+  def install
+    mkdir "build" do
+      args = std_cmake_args
+      args << "-DALT_CMAKE=ON"
+      system "cmake", "..", *args
+      system "make"
+      system "ctest"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include "canvas/Version/GetReleaseVersion.h"
+      #include <iostream>
+      int main() {
+        std::cout << art::getCanvasReleaseVersion() << std::endl;;
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++1y", "test.cpp", "-L#{lib}", "-lcanvas", "-o", "test"
+    system "./test"
+  end
+end
