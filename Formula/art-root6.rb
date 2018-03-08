ac870cf587102d3fd29fc2eea0e9ad2212807d24
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-08 16:30:27 +0000

    art-root6: Update in line with upstream homebrew
    Use Python3 by default, retaining python@2 option. Take Python
    selection and configuration from upstream root.rb formula.
    
    Make sqlite, ssl, gsl use required. Add dependency on art-tbb
    for now default MT mode.

--- Formula/art-root6.rb
+++ Formula/art-root6.rb
@@ -9,16 +9,14 @@ class ArtRoot6 < Formula
   head "http://root.cern.ch/git/root.git"
 
   depends_on "cmake" => :build
-  depends_on "openssl" => :recommended
-  depends_on "sqlite" => :recommended
-  depends_on "python@2" => :recommended
-  depends_on "gsl" => :recommended
-
-  # For LZMA
-  depends_on "xz"
-
-  # For XML on Linux
-  depends_on "libxml2" unless OS.mac?
+  depends_on "art-tbb"
+  depends_on "xz" # For LZMA
+  depends_on "libxml2" unless OS.mac? # For XML on Linux
+  depends_on "openssl"
+  depends_on "sqlite"
+  depends_on "gsl"
+  depends_on "python" => :recommended
+  depends_on "python@2" => :optional
 
   conflicts_with "root", :because => "Art suite needs c++14"
 
@@ -35,64 +33,107 @@ class ArtRoot6 < Formula
     # brew audit doesn't like non-executables in bin
     # so we will move {thisroot,setxrd}.{c,}sh to libexec
     # (and change any references to them)
-    inreplace Dir["config/roots.in", "config/thisroot.*sh",
-                  "etc/proof/utils/pq2/setup-pq2",
-                  "man/man1/setup-pq2.1", "README/INSTALL", "README/README"],
-      /bin.thisroot/, "libexec/thisroot"
+    #inreplace Dir["config/roots.in", "config/thisroot.*sh",
+    #              "etc/proof/utils/pq2/setup-pq2",
+    #              "man/man1/setup-pq2.1", "README/INSTALL", "README/README"],
+    #  /bin.thisroot/, "libexec/thisroot"
+
+    args = *std_cmake_args
+    args << "-DCMAKE_ELISPDIR=#{elisp}"
+
+    # Disable everything that might be ON by default
+    args = args + %W[
+      -Dalien=OFF
+      -Dasimage=OFF
+      -Dastiff=OFF
+      -Dbonjour=OFF
+      -Dcastor=OFF
+      -Dchirp=OFF
+      -Ddavix=OFF
+      -Ddcache=OFF
+      -Dfitsio=OFF
+      -Dfortran=OFF
+      -Dgfal=OFF
+      -Dglite=OFF
+      -Dgviz=OFF
+      -Dhdfs=OFF
+      -Dkrb5=OFF
+      -Dldap=OFF
+      -Dmonalisa=OFF
+      -Dmysql=OFF
+      -Dodbc=OFF
+      -Doracle=OFF
+      -Dpgsql=OFF
+      -Dpythia6=OFF
+      -Dpythia8=OFF
+      -Dqt=OFF
+      -Drfio=OFF
+      -Dsapdb=OFF
+      -Dsrp=OFF
+      -Dunuran=OFF
+    ]
+
+    # Now the core/builtin things we want
+    args = args + %W[
+      -Dcxx14=ON
+      -Dfail-on-missing=ON
+      -Dgnuinstall=ON
+      -Dexplicitlink=ON
+      -Drpath=ON
+      -Dsoversion=ON
+      -Dbuiltin_asimage=ON
+      -Dasimage=ON
+      -Dbuiltin_fftw3=ON
+      -Dbuiltin_freetype=ON
+      -Droofit=ON
+      -Dgdml=ON
+      -Dminuit2=ON
+    ]
+
+    # Options that require an external
+    args = args + %W[
+      -Dsqlite=ON
+      -Dssl=ON
+      -Dmathmore=ON
+      -Dxrootd=OFF
+    ]
+
+    # Python requires a bit of finessing
+    if build.with?("python") && build.with?("python@2")
+       odie "Root: Does not support building both python 2 and 3 wrappers"
+    elsif build.with?("python") || build.with?("python@2")
+      if build.with? "python@2"
+        ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
+        python_executable = Utils.popen_read("which python").strip
+        python_version = Language::Python.major_minor_version("python")
+      elsif build.with? "python"
+        python_executable = Utils.popen_read("which python3").strip
+        python_version = Language::Python.major_minor_version("python3")
+      end
+
+      python_prefix = Utils.popen_read("#{python_executable} -c 'import sys;print(sys.prefix)'").chomp
+      python_include = Utils.popen_read("#{python_executable} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))'").chomp
+      args << "-Dpython=ON"
+
+      # cmake picks up the system's python dylib, even if we have a brewed one
+      if File.exist? "#{python_prefix}/Python"
+        python_library = "#{python_prefix}/Python"
+      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.a"
+        python_library = "#{python_prefix}/lib/lib#{python_version}.a"
+      elsif File.exist? "#{python_prefix}/lib/lib#{python_version}.dylib"
+        python_library = "#{python_prefix}/lib/lib#{python_version}.dylib"
+      else
+        odie "No libpythonX.Y.{a,dylib} file found!"
+      end
+      args << "-DPYTHON_EXECUTABLE='#{python_executable}'"
+      args << "-DPYTHON_INCLUDE_DIR='#{python_include}'"
+      args << "-DPYTHON_LIBRARY='#{python_library}'"
+    else
+      args << "-Dpython=OFF"
+    end
 
     mkdir "cmake-build" do
-      system "cmake", "..",
-        # Disable everything that might be ON by default...
-        # minimal/gminimal don't allow override...
-        "-Dalien=OFF",
-        "-Dasimage=OFF",
-        "-Dastiff=OFF",
-        "-Dbonjour=OFF",
-        "-Dcastor=OFF",
-        "-Dchirp=OFF",
-        "-Ddavix=OFF",
-        "-Ddcache=OFF",
-        "-Dfitsio=OFF",
-        "-Dfortran=OFF",
-        "-Dgfal=OFF",
-        "-Dglite=OFF",
-        "-Dgviz=OFF",
-        "-Dhdfs=OFF",
-        "-Dkrb5=OFF",
-        "-Dldap=OFF",
-        "-Dmonalisa=OFF",
-        "-Dmysql=OFF",
-        "-Dodbc=OFF",
-        "-Doracle=OFF",
-        "-Dpgsql=OFF",
-        "-Dpythia6=OFF",
-        "-Dpythia8=OFF",
-        "-Dqt=OFF",
-        "-Drfio=OFF",
-        "-Dsapdb=OFF",
-        "-Dsrp=OFF",
-        # Now the stuff we want
-        "-Dcxx14=ON",
-        "-Dfail-on-missing=ON",
-        "-Dgnuinstall=ON",
-        "-Dexplicitlink=ON",
-        "-Drpath=ON",
-        "-Dsoversion=ON",
-        "-Dbuiltin_asimage=ON",
-        "-Dasimage=ON",
-        "-Dbuiltin_fftw3=ON",
-        "-Dbuiltin_freetype=ON",
-        "-Droofit=ON",
-        "-Dgdml=ON",
-        "-Dminuit2=ON",
-        # MT is on by default, use builtin
-        "-Dbuiltin_tbb=ON",
-        cmake_opt("python"),
-        cmake_opt("ssl", "openssl"),
-        cmake_opt("sqlite", "sqlite3"),
-        cmake_opt("xrootd"),
-        cmake_opt("mathmore", "gsl"),
-        *std_cmake_args
+      system "cmake", "..", *args
 
       # Follow upstream homebrew
       # Work around superenv stripping out isysroot leading to errors with
@@ -103,10 +144,9 @@ class ArtRoot6 < Formula
       else
         system "make", "install"
       end
-    end
 
-    libexec.mkpath
-    mv Dir["#{bin}/*.*sh"], libexec
+      chmod 0755, Dir[bin/"*.*sh"]
+    end
   end
 
   def caveats; <<~EOS
@@ -117,11 +157,11 @@ class ArtRoot6 < Formula
     before using ROOT.
 
     For bash users:
-      . $(brew --prefix root6)/libexec/thisroot.sh
+      . #{HOMEBREW_PREFIX}/bin/thisroot.sh
     For zsh users:
-      pushd $(brew --prefix root6) >/dev/null; . libexec/thisroot.sh; popd >/dev/null
+      pushd #{HOMEBREW_PREFIX} >/dev/null; . bin/thisroot.sh; popd >/dev/null
     For csh/tcsh users:
-      source `brew --prefix root6`/libexec/thisroot.csh
+      source #{HOMEBREW_PREFIX}/bin/thisroot.csh
     EOS
   end
 
@@ -133,10 +173,15 @@ class ArtRoot6 < Formula
       }
     EOS
     (testpath/"test.bash").write <<~EOS
-      . #{libexec}/thisroot.sh
+      . #{bin}/thisroot.sh
       root -l -b -n -q test.C
     EOS
     assert_equal "\nProcessing test.C...\nHello, world!\n",
-      `/bin/bash test.bash`
+                 shell_output("/bin/bash test.bash")
+
+    if build.with? "python"
+      ENV["PYTHONPATH"] = lib/"root"
+      system "python3", "-c", "import ROOT"
+    end
   end
 end
