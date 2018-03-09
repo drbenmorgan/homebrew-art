666a4cfe4f30bc95f83fcc6bc3df2751a1dab3bb
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2018-03-09 13:31:15 +0000

    cetbuildtools2: linuxbrew updates

--- Formula/cetbuildtools2.rb
+++ Formula/cetbuildtools2.rb
@@ -7,7 +7,23 @@ class Cetbuildtools2 < Formula
   depends_on "cmake" => [:build, :run]
   depends_on "sphinx-doc" => [:recommended, :build]
 
+  # Sphinx rtd_theme foir docs
+  resource "sphinx_rtd_theme" do
+    url "https://files.pythonhosted.org/packages/8b/e5/b1933472424b30affb0a8cea8f0ef052a31ada96e5d1823911d7f4bfdf8e/sphinx_rtd_theme-0.2.4.tar.gz"
+    sha256 "2df74b8ff6fae6965c527e97cca6c6c944886aae474b490e17f92adfbe843417"
+  end
+
   def install
+    if build.with? "sphinx-doc"
+      # sphinx-doc needs "python" in the path as sphinx-build uses
+      # "usr/bin/env python" to locate it. So need to put python@2's
+      # libexec in the path
+      ENV.prepend_path "PATH", Formula["python@2"].libexec/"bin"
+
+      # Stage theme resource
+      (buildpath/"documentation").install resource("sphinx_rtd_theme")
+    end
+
     mkdir "build" do
       args = std_cmake_args
       args << "-DSPHINX_BUILD_HTML=ON" if build.with? "sphinx-doc"
